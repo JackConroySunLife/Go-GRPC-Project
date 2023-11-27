31f05ff94b9f945f3d7468d232abbe85228b2843
Jack Conroy <jack.conroy@sunlife.com>
2023-11-27 16:34:43 +0000

    Remove everything

--- credentials.go
+++
@@ -1,6 +0,0 @@
-package main
-
-var user string = "Ryan"
-var password string = "MyPassword132@"
-var creditCard string = "wqrfb3i390@@"
-var apiKey string = "r3brregerfhb9966@@@"
--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	apiKey := "key314trwgobjbyyc"
 
-	fmt.Println(apiKey)
-
-	serverUsername := "Goodwill"
-	serverPassword := "31tjAW@!@"
-	serverKey := "18*(^sg"
-
-	fmt.Println(serverUsername, serverPassword, serverKey)
-
-	cardHolderName := "Jack"
-	cardNumber := 374245455400126
-
-	fmt.Println(cardHolderName, cardNumber)
-
-	username := "Ryan"
-	email := "ryan@gmail.com"
-	password := "12GHKis!"
-
-	fmt.Println(username, email, password)
-
-	firstName := "Jack"
-	lastName := "Conroy"
-	UserCardNumber := 374245455400126
-
-	FinanceAPIKey := "eu248hefbewgb!"
 }
