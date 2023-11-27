69674af7cede9ec02384485813349cf70f4bb9e6
Jack Conroy <jack.conroy@sunlife.com>
2023-11-27 16:43:35 +0000

    Add server creds

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	remoteServerUser := "Ryan"
+	remoteServerPassword := "e2528@@3£4hv"
+
+	fmt.Println(remoteServerPassword, remoteServerUser)
 }
